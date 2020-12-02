b63424b2ee825d768d854513f589968029bb3688
syarifse7 <syarifudinse7@gmail.com>
2020-12-02 22:52:09 +0700

    add jquery file and jquery-easing

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+/*$(document).ready(function() {
+
+	alert('ready to use!');
+
+});*/
+
+$('.page-scroll').on('click', function() {
+	/* ambil isi href */
+	let tujuan = $(this).attr('href');
+
+	/* trigger element parent href */
+	let parentElement = $(tujuan);
+
+	/* pindahkan scroll */
+	$('html, body').animate({
+		scrollTop : parentElement.offset().top -60
+	}, 1100, 'easeInOutExpo');
+
+});
+
 /*let user = prompt("What's your name?");
 
 	alert('hallo ' + user + ' nice to meet you ');*/
 
 /* add function */
 
-const changeJobsTitle = (job) => {
+/*const changeJobsTitle = (job) => {
 	const JobsTitle = document.getElementsByTagName('h5');
 
 	if (typeof job === 'string'){
@@ -16,9 +36,9 @@ const changeJobsTitle = (job) => {
 	}
 
 }
-
+*/
 // call function 
-changeJobsTitle('Front-End Developer | Cat Lovers');
+/*changeJobsTitle('Front-End Developer | Cat Lovers');
  
 const pagescrolledAboutMe = document.querySelector('#about');
 
@@ -26,4 +46,4 @@ pagescrolledAboutMe.addEventListener('click', function(){
 
 	alert('oke!');
 
-});
+});*/
